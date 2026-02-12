260548e8e8a98d01e883330f3dfac95bbab7b102
LuizGoulart-RTech <luiz.goulart@rtechsolution.com.br>
2026-02-12 07:45:16 -0300

    fix(api): atualizando base url em homolog

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.semensol.rtechsolution.com.br/api';
+const BASE_URL = 'https://develop.api.semensol.rtechsolution.com.br/api';
 
 interface ApiResponse<T> {
   data?: T;
